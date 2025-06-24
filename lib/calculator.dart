1d8d2fbbdb4d836e36974a941ad85d407f04fcb8
roja.patil <roja.patil@greytrixindia.com>
2025-06-24 15:00:12 +0530

    returns the number itself

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,7 +1,11 @@
 class Calculator {
   static int add(String numbers) {
     /// step 1.1 Input: “”, Output: 0
+    // if (numbers.isEmpty) return 0;
+    // return 0;
+
+    ///step1.2 Input: “1”, Output: 1
     if (numbers.isEmpty) return 0;
-    return 0;
+    return int.parse(numbers);
   }
 }
--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -5,8 +5,13 @@ void main(){
   group("calculator", ()
   {
     /// step 1.1 Input: “”, Output: 0
-    test('return 0 for empty string', () {
-      expect(Calculator.add(""), 0);
+    // test('return 0 for empty string', () {
+    //   expect(Calculator.add(""), 0);
+    // });
+
+    ///step1.2 Input: “1”, Output: 1
+    test('returns the number itself', () {
+      expect(Calculator.add("4"), 4);
     });
   });
 }
